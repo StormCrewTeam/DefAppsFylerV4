933e3b038a54b09c7d2a14db923e96070837bb84
Alex's Mac <>
2020-07-17 15:29:28 +0300

    version 1.13

--- defappsflyer/src/java/DefAppsFlyer.java
+++ defappsflyer/src/java/DefAppsFlyer.java
@@ -12,47 +12,31 @@ public class DefAppsFlyer {
     static String conversionResult = "Pepe";
 
     public static void DefAppsFlyer_setAppsFlyerKey(final Activity appActivity, final String appsFlyerKey) {
-        conversionResult = "PEPONETE";
         AppsFlyerConversionListener conversionDataListener =
                 new AppsFlyerConversionListener() {
 
-                    // @Override
-                    // public void onConversionDataSuccess(Map<String, Object> conversionData) {
-                    //     conversionResult = "conversion data success";
-                    //     //     for (String attrName : conversionData.keySet()) {
-                    //     //     Log.d("LOG_TAG", "attribute: " + attrName + " = " + conversionData.get(attrName));
-                    //     // }
-                    // }
-
-                    // @Override
-                    // public void onConversionDataFail(String errorMessage) {
-                    //     conversionResult = "conversion data error";
-                    //     // Log.d("LOG_TAG", "error getting conversion data: " + errorMessage);
-                    // }
-
                     @Override
                     public void onAppOpenAttribution(Map<String, String> attributionData) {
-                        conversionResult = "attribution data success";
-                        // for (String attrName : attributionData.keySet()) {
-                        //     Log.d("LOG_TAG", "attribute: " + attrName + " = " + attributionData.get(attrName));
-                        // }
-
+                        for (String attrName : attributionData.keySet()) {
+                            conversionDataListener += "attribute: " + attrName + " = " + attributionData.get(attrName);
+                        }
                     }
 
                     @Override
                     public void onAttributionFailure(String errorMessage) {
-                        conversionResult = "attribution data error";
-                        // Log.d("LOG_TAG", "error onAttributionFailure : " + errorMessage);
+                        conversionResult = "error";
                     }
 
                     @Override
                     public void onInstallConversionDataLoaded(java.util.Map<java.lang.String, java.lang.String> conversionData) {
-                        conversionResult = "Got install conversion";
+                        for (String attrName : attributionData.keySet()) {
+                            conversionDataListener += "attribute: " + attrName + " = " + attributionData.get(attrName);
+                        }
                     }
 
                     @Override
                     public void onInstallConversionFailure(String errorMessage) {
-                        conversionResult = "Got install conversion error";
+                        conversionResult = "error";
                     }
                 };
         AppsFlyerLib.getInstance().init(appsFlyerKey, conversionDataListener, appActivity.getApplicationContext());
