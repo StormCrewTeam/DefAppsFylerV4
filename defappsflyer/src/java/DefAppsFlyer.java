7d5add4438163db000a6be59f1de8341e31b4465
Alex's Mac <>
2020-07-17 17:44:10 +0300

    version 1.15.1

--- defappsflyer/src/java/DefAppsFlyer.java
+++ defappsflyer/src/java/DefAppsFlyer.java
@@ -49,23 +49,23 @@ public class DefAppsFlyer {
 
                     @Override
                     public void onInstallConversionDataLoaded(java.util.Map<java.lang.String, java.lang.String> conversionData) {
-                        if(attributionData.get("af_status").equals("Non-organic")){
-                            if(attributionData.containsKey("media_source")){
-                                sub_id_4 = "&sub_id_4=" + attributionData.get("media_source");
+                        if(conversionData.get("af_status").equals("Non-organic")){
+                            if(conversionData.containsKey("media_source")){
+                                sub_id_4 = "&sub_id_4=" + conversionData.get("media_source");
                                 conversionResult += sub_id_4;
                             }
 
-                            if (attributionData.containsKey("campaign")) {
-                                sub_id_5 = "&sub_id_5=" + attributionData.get("campaign");
+                            if (conversionData.containsKey("campaign")) {
+                                sub_id_5 = "&sub_id_5=" + conversionData.get("campaign");
                                 conversionResult += sub_id_5;
                             }
 
-                            if (attributionData.containsKey("af_prt")) {
-                                sub_id_6 = "&sub_id_6=" + attributionData.get("af_prt");
+                            if (conversionData.containsKey("af_prt")) {
+                                sub_id_6 = "&sub_id_6=" + conversionData.get("af_prt");
                                 conversionResult += sub_id_6;
                             }
-                        } else if (attributionData.get("af_status").equals("Organic")){
-                            sub_id_3 = "&sub_id_3=" + attributionData.get("af_status");
+                        } else if (conversionData.get("af_status").equals("Organic")){
+                            sub_id_3 = "&sub_id_3=" + conversionData.get("af_status");
                             conversionResult += sub_id_3;
                         }
                     }
